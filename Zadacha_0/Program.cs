81d6b2d703e4e4b422e176173286e8d8705816d6
Asus <koncarik@mail.ru>
2022-11-27 21:00:21 +0500

    AllCorrect

---
+++ Zadacha_0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int square = number*number;
+Console.WriteLine($"Квадрат числа {number} равен {square}");
+//comment
